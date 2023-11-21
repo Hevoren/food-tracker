a8a9fe64559f03f5662482f60cd6df4e8373b22f
Hevoren <fifaultimateamfifa15@gmail.com>
2023-11-21 16:48:05 +0700

    [added] saving or updating data to localstorage with unique autoincrement key

--- src/components/Main/AddProduct.vue
+++ src/components/Main/AddProduct.vue
@@ -1,32 +1,34 @@
 <script>
-import { setItem, getItem } from "@/helpers/persistanceStorage";
+import {getItem, setItem} from "@/helpers/persistanceStorage";
 
 export default {
   name: 'AddProduct',
   props: {
     formVisible: {
       type: Boolean,
-          required: true
+      required: true
     }
   },
   data() {
     return {
-      products: {
+      products: [],
+      newProduct: {
         title: '',
         kcal: '',
         weight: '',
         proteins: '',
         fats: '',
         carbohydrates: '',
-      }
+      },
+      tempProducts: []
     }
   },
   methods: {
     closeForm() {
       this.emitter.emit('formVisible', false)
     },
-    onSubmit() {
-      this.products = {
+    clearFrom() {
+      this.newProduct = {
         title: '',
         kcal: '',
         weight: '',
@@ -34,22 +36,57 @@ export default {
         fats: '',
         carbohydrates: '',
       };
+      localStorage.removeItem('product')
+    },
+    onSubmit() {
+      const newProductCopy = { ...this.newProduct }; // Создание копии продукта
+      this.products.push({
+        id: this.autoIncrementMaker(),
+        ...newProductCopy,
+      });
+      console.log(this.products)
+      this.saveProduct()
+      this.clearFrom()
       this.closeForm()
     },
-    saveProductToLocalStorage() {
-      setItem('products', JSON.stringify(this.products))
+    tempSaveProduct() {
+      setItem('product', JSON.stringify(this.newProduct))
+    },
+    tempLoadProduct() {
+      const savedProducts = getItem('product');
+      if (savedProducts) {
+        this.newProduct = JSON.parse(savedProducts);
+      }
+    },
+    saveProduct() {
+      const existingProducts = getItem('products')
+      let productsArray = [];
+      if (existingProducts) {
+        productsArray = JSON.parse(existingProducts);
+      }
+      productsArray.push(...this.products);
+      setItem('products', JSON.stringify(productsArray));
+    },
+    autoIncrementMaker() {
+      const existingProducts = getItem('products')
+      if (existingProducts) {
+        this.tempProducts = JSON.parse(existingProducts);
+        console.log(this.tempProducts.id)
+        const maxId = this.tempProducts.reduce((max, product) => (product.id > max ? product.id : max), 0);
+        return maxId + 1;
+      }
+      return 1
+
     }
   },
   watch: {
-    products: {
-      handler: 'saveProductToLocalStorage',
+    newProduct: {
+      handler: 'tempSaveProduct',
       deep: true
     }
   },
   mounted() {
-    if (getItem('products')) {
-      this.products = JSON.parse(getItem('products'))
-    }
+    this.tempLoadProduct()
   }
 }
 </script>
@@ -58,12 +95,12 @@ export default {
   <div @click="closeForm" :class="{ 'form-product-bg': this.formVisible }">
     <div class="form-product" @click.stop>
       <form action="/" @submit.prevent="onSubmit">
-        <input type="text" name="input" placeholder="Название" v-model="products.title">
-        <input type="text" name="kcal" placeholder="Каллорийность" v-model="products.kcal">
-        <input type="text" name="weight" placeholder="Вес" v-model="products.weight">
-        <input type="text" name="protein" placeholder="Белки" v-model="products.proteins">
-        <input type="text" name="fats" placeholder="Жиры" v-model="products.fats">
-        <input type="text" name="carbohydrates" placeholder="Углеводы" v-model="products.carbohydrates">
+        <input type="text" name="input" placeholder="Название" v-model="newProduct.title">
+        <input type="text" name="kcal" placeholder="Каллорийность" v-model="newProduct.kcal">
+        <input type="text" name="weight" placeholder="Вес" v-model="newProduct.weight">
+        <input type="text" name="protein" placeholder="Белки" v-model="newProduct.proteins">
+        <input type="text" name="fats" placeholder="Жиры" v-model="newProduct.fats">
+        <input type="text" name="carbohydrates" placeholder="Углеводы" v-model="newProduct .carbohydrates">
         <button type="submit">Add</button>
       </form>
     </div>
